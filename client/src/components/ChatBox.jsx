ae95f0840e873b925066dcf1f5b5493053a2a0be
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-07-04 11:33:41 +0530

    setup ChatBox with conversation state and welcome screen

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
+import logo_light from "../assets/logo_light.png";
+import logo_dark from "../assets/logo_dark.png";
 
 const ChatBox = () => {
-  return (
-    <div>ChatBox</div>
-  )
-}
+  const { selectedChat, theme } = useAppContext();
 
-export default ChatBox
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (selectedChat) {
+      setMessages(selectedChat.messages);
+    }
+  }, [selectedChat]);
+
+  return (
+    <div className="flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
+      {/* Chat Messages */}
+      <div className="flex-1 mb-5 overflow-y-scroll">
+        {messages.length === 0 && (
+          <div className="h-full flex flex-col items-center justify-center gap-2 text-primary">
+            <img
+              src={theme === "dark" ? logo_light : logo_dark}
+              alt="logo"
+              className="w-full max-w-56 sm:max-w-68"
+            />
+            <p className="mt-5 text-4xl sm:text-6xl text-center text-gray-700 dark:text-white">
+              Ask me anything.
+            </p>
+          </div>
+        )}
+      </div>
+
+      {/* Promp Input Box */}
+      <form></form>
+    </div>
+  );
+};
+
+export default ChatBox;
