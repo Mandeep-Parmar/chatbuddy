442f96d14aad07272f3d0904d5d87918ba5cf87f
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-07-05 13:44:00 +0530

    implement community gallery for published AI images

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { dummyPublishedImages } from "../assets/assets";
+import Loading from "./Loading";
 
 const Community = () => {
-  return (
-    <div>Community</div>
-  )
-}
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-export default Community
+  const fetchImages = async () => {
+    setLoading(true);
+
+    // API call...
+
+    setImages(dummyPublishedImages);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  if (loading) return <Loading />;
+
+  return (
+    <div className="flex-1 h-full overflow-y-auto p-6 pt-12 xl:px-12 2xl:px-20">
+      <h2 className="text-xl font-semibold mb-6 text-gray-800 dark:text-white">
+        Community Images
+      </h2>
+
+      {images.length > 0 ? (
+        <div className="flex flex-wrap max-sm:justify-center gap-5">
+          {images.map((img, index) => (
+            <a
+              key={index}
+              href={img.imageUrl}
+              target="_blank"
+              className="relative group block rounded-lg overflow-hidden border border-gray-200 dark:border-purple-700 shadow-sm hover:shadow-md transition-shadow duration-300"
+            >
+              <img
+                src={img.imageUrl}
+                alt="image"
+                className="w-full h-40 md:h-50 2xl:h-62 object-cover group-hover:scale-105 transition-transform duration-300 ease-in-out"
+              />
+
+              <p className="absolute bottom-0 right-0 text-xs bg-black/50 backdrop-blur-2xl text-white px-4 py-1 rounded-tl-xl">
+                Created by {img.userName}
+              </p>
+            </a>
+          ))}
+        </div>
+      ) : (
+        <p className="text-center text-gray-600 dark:text-purple-200 mt-10">
+          No Images Available.
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Community;
